fa90c743f0fa568776c3b2cbcfec9b699f123c5b
Phil <pmash2@users.noreply.github.com>
2021-10-17 17:40:24 -0400

    Create game screen

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+# https://www.edureka.co/blog/snake-game-with-pygame/
+
+pygame.init()
+dis = pygame.display.set_mode((400, 300))
+pygame.display.update()
+
+pygame.display.set_caption('Snake game by pmash')
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        print(event)
+
+pygame.quit()
+quit()
