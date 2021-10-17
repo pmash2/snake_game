7bc14b8dc14dc95bb2316ead3d7ada2d8ec0c0fa
Phil <pmash2@users.noreply.github.com>
2021-10-17 17:41:20 -0400

    Quit game when x is clicked

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ pygame.display.set_caption('Snake game by pmash')
 game_over = False
 while not game_over:
     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
         print(event)
 
 pygame.quit()
