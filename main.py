fd5d3891b14d6bd8eab66d00663b869c3b7d381e
Phil <pmash2@users.noreply.github.com>
2021-10-17 17:51:43 -0400

    Create the snake

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ dis = pygame.display.set_mode((400, 300))
 pygame.display.update()
 
 pygame.display.set_caption('Snake game by pmash')
+
+color_blue = (0, 0, 255)
+color_red = (255, 0, 0)
+
 game_over = False
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
         print(event)
+
+        pygame.draw.rect(dis, color_blue, [200, 150, 10, 10])
+        pygame.display.update()
 
 pygame.quit()
 quit()
