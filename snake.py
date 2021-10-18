5ca2ebccb6081d065ebae4de561dbc3d435dc1d3
Phil <pmash2@users.noreply.github.com>
2021-10-18 00:16:35 -0400

    Create helper funtion in snake class

--- snake.py
+++ snake.py
@@ -14,16 +14,12 @@ class Snake:
         self.Speed = 15
 
     def add_node(self, x, y):
-        node = []
-        node.append(x)
-        node.append(y)
+        node = self.make_node(x, y)
         self.Nodes.append(node)
 
     # BUG: This is not working right...
     def collided_with_self(self, x, y):
-        currentNode = []
-        currentNode.append(x)
-        currentNode.append(y)
+        currentNode = self.make_node(x, y)
 
         # Everything but the last item in the list
         for x in self.Nodes[:-1]:
@@ -36,3 +32,9 @@ class Snake:
         snake_block = _settings.Snake_Block_Size
         for coord in self.Nodes:
             pygame.draw.rect(_surface, MyColors.Blue, [int(coord[0]), int(coord[1]), snake_block, snake_block])
+
+    def make_node(x, y):
+        node = []
+        node.append(x)
+        node.append(y)
+        return node
