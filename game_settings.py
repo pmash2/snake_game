f0a8fd92196102a228f8d35328c29afd5af70d77
Phil <pmash2@users.noreply.github.com>
2021-10-17 22:28:28 -0400

    Extract colors and settings to separate files

---
+++ game_settings.py
@@ -0,0 +1,10 @@
+import pygame
+
+
+class Settings:
+    Display_Height = 400
+    Display_Width = 600
+
+    def __init__(self) -> None:
+        self.Score_Font = pygame.font.SysFont("comicsansms", 35)
+        self.Status_Font = pygame.font.SysFont(None, 50)
--- main.py
+++ main.py
@@ -1,55 +1,49 @@
 import pygame
 import time
 import random
+from colors import Colors as MyColors
+from game_settings import Settings
 
 # https://www.edureka.co/blog/snake-game-with-pygame/
 
 pygame.init()
 
-color_white = (255, 255, 255)
-color_light_blue = (50, 153, 213)
-color_red = (255, 0, 0)
-color_blue = (0, 0, 255)
-color_yellow = (255, 255, 102)
-color_green = (0, 255, 0)
+_settings = Settings()
 
 snake_block = 10
 snake_speed = 15
 
-display_height = 400
-display_width = 600
-
-dis = pygame.display.set_mode((display_width, display_height))
+dis = pygame.display.set_mode(
+    (_settings.Display_Width, _settings.Display_Height))
 pygame.display.set_caption('Snake game by pmash')
 
 clock = pygame.time.Clock()
 
-font_style = pygame.font.SysFont(None, 50)
-score_font = pygame.font.SysFont("comicsansms", 35)
-
 
 def your_score(score):
-    value = score_font.render(f"Your Score: {score}", True, color_yellow)
+    value = _settings.Score_Font.render(
+        f"Your Score: {score}", True, MyColors.Yellow)
     dis.blit(value, [0, 0])
 
 
 def our_snake(snake_block, snake_list):
     for coord in snake_list:
-        pygame.draw.rect(dis, color_blue, [
+        pygame.draw.rect(dis, MyColors.Blue, [
                          coord[0], coord[1], snake_block, snake_block])
 
 
 def message(msg, color):
-    _message = font_style.render(msg, True, color)
-    dis.blit(_message, [display_width / 2, display_height / 2])
+    _message = _settings.Status_Font.render(msg, True, color)
+    dis.blit(_message, [_settings.Display_Width /
+             2, _settings.Display_Height / 2])
 
 
 def gameLoop():
     game_over = False
     game_close = False
 
-    x1 = display_width / 2
-    y1 = display_height / 2
+    x1 = _settings.Display_Width / 2
+    y1 = _settings.Display_Height / 2
     x1_change = 0
     y1_change = 0
 
@@ -57,15 +51,15 @@ def gameLoop():
     snake_length = 1
 
     foodx = round(random.randrange(
-        0, display_width - snake_block) / 10.0) * 10.0
+        0, _settings.Display_Width - snake_block) / 10.0) * 10.0
     foody = round(random.randrange(
-        0, display_height - snake_block) / 10.0) * 10.0
+        0, _settings.Display_Height - snake_block) / 10.0) * 10.0
 
     while not game_over:
 
         while game_close == True:
-            dis.fill(color_light_blue)
-            message("You Lost! Press Q-Quit or C-Play Again", color_red)
+            dis.fill(MyColors.Light_Blue)
+            message("You Lost! Press Q-Quit or C-Play Again", MyColors.Red)
             your_score(snake_length - 1)
             pygame.display.update()
 
@@ -96,13 +90,13 @@ def gameLoop():
                     x1_change = 0
                     y1_change = snake_block
 
-        if x1 >= display_width or x1 < 0 or y1 >= display_height or y1 < 0:
+        if x1 >= _settings.Display_Width or x1 < 0 or y1 >= _settings.Display_Height or y1 < 0:
             game_close = True
 
         x1 += x1_change
         y1 += y1_change
-        dis.fill(color_light_blue)
-        pygame.draw.rect(dis, color_green, [
+        dis.fill(MyColors.Light_Blue)
+        pygame.draw.rect(dis, MyColors.Green, [
                          foodx, foody, snake_block, snake_block])
 
         snake_head = []
@@ -123,9 +117,9 @@ def gameLoop():
 
         if x1 == foodx and y1 == foody:
             foodx = round(random.randrange(
-                0, display_width - snake_block) / 10.0) * 10.0
+                0, _settings.Display_Width - snake_block) / 10.0) * 10.0
             foody = round(random.randrange(
-                0, display_height - snake_block) / 10.0) * 10.0
+                0, _settings.Display_Height - snake_block) / 10.0) * 10.0
             snake_length += 1
 
         clock.tick(snake_speed)
